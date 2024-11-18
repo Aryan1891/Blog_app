aa2efa13bba4aa4d8395c5d408548f59e886b9b9
Aryan1891 <aryanjangirofficial@gmail.com>
2024-11-19 00:21:42 +0530

    add login and registration functionality

--- api/index.js
+++ api/index.js
@@ -1,11 +1,59 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
+const User = require('./models/User');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 const app = express();
+const bcrypt = require('bcryptjs');
+
+const salt=bcrypt.genSaltSync(10);
+const secret="asdfasdfasndfklasdfrdflsadsf";
+
+
 app.use(cors({credentials:true,origin:'http://localhost:3000'}));
 app.use(express.json());
 
-app.get('/test', (req, res) => {
-  res.json('Hello forld!');
-});
+
+
+ mongoose.connect('mongodb+srv://aryanjangirofficial:aryanjangir@cluster0.cf2sx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+);
+
+
+app.post('/register', async (req,res) => {
+    const {username,password} = req.body;
+    try{
+      const userDoc = await User.create({
+        username,
+        password:bcrypt.hashSync(password,salt),
+      });
+      res.json(userDoc);
+    } catch(e) {
+      console.log(e);
+      res.status(400).json(e);
+    }
+  });
+  
+  app.post('/login', async (req,res) => {
+    const {username,password} = req.body;
+    const userDoc = await User.findOne({username});
+    const passOk = bcrypt.compareSync(password, userDoc.password);
+    if (passOk) {
+      jwt.sign({username,id:userDoc._id}, secret, {}, (err,token) => {
+        if (err) throw err;
+        res.cookie('token', token).json({
+          id:userDoc._id,
+          username,
+        });
+      });
+    } else {
+      res.status(400).json('wrong credentials');
+    }
+  });
+
+
+
+
 
 app.listen(4000);
+
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,49 @@
-import React from 'react'
+import {Navigate} from "react-router-dom";
+import React, { useState } from 'react'
 
 const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [redirect, setRedirect] = useState(false);
+
+  async function login(ev) {
+    ev.preventDefault();
+    
+    const response = await fetch('http://localhost:4000/login',{
+      method: 'POST',
+      body: JSON.stringify({username, password}),
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      credentials: 'include',
+    })
+
+    if(response.ok){
+      setRedirect(true);
+    }
+    else{
+      alert('login failed');
+    }
+   
+
+  }
+  if(redirect){
+    return <Navigate to={'/'}/>
+  }
   return (
-    <div className='login-card'>
-        <form className='login'> 
+    <div className='login-card' >
+        <form className='login' onSubmit={login}> 
             <h1>Login Now</h1>
-            <input type="text" placeholder='Your Username'/>
-            <input type="password" placeholder='Your Password'/>
+            <input type="text"
+            placeholder='Your Username'
+            value={username}
+            onChange={ev => setUsername(ev.target.value)}
+            />
+            <input type="password" 
+            placeholder='Your Password'
+            value={password}
+            onChange={ev => setPassword(ev.target.value)}
+            />
             <button class="button-17">Login</button>
         </form>
     </div>
